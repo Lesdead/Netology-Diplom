cb79c6d2cafbebeb3111bdaa010b0deeb5c0b5df
lesdead <lesdead@gmail.com>
2023-02-10 03:20:44 +0200

    shouldNewsMenuDisplayed test added

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/tests/newsTests/NewsMenuDisplayed.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/tests/newsTests/NewsMenuDisplayed.java
@@ -1,4 +1,57 @@
 package ru.iteco.fmhandroid.ui.tests.newsTests;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static ru.iteco.fmhandroid.ui.helper.MainHelper.elementWaiting;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ru.iteco.fmhandroid.R;
+import ru.iteco.fmhandroid.ui.AppActivity;
+import ru.iteco.fmhandroid.ui.screenElements.AboutAppScreen;
+import ru.iteco.fmhandroid.ui.screenElements.AuthorizationScreen;
+import ru.iteco.fmhandroid.ui.screenElements.MainScreen;
+import ru.iteco.fmhandroid.ui.screenElements.NewsScreen;
+import ru.iteco.fmhandroid.ui.steps.AboutSteps;
+import ru.iteco.fmhandroid.ui.steps.NewsSteps;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
 public class NewsMenuDisplayed {
+
+    @Rule
+    public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(AppActivity.class);
+
+    @Before
+    public void logInCheck() {
+        try {
+            Thread.sleep(3000);
+            AuthorizationScreen.authorization.check(matches(isDisplayed()));
+        } catch (Exception e) {
+            MainScreen.authorizationButton.perform(click());
+            MainScreen.logOutButton.perform(click());
+        }
+    }
+
+    @Test
+    public void shouldNewsMenuDisplayed() throws InterruptedException {
+        NewsSteps.enterNewsMenu();
+        NewsScreen.titleOfNewsBlock.check(matches(isDisplayed()));
+    }
 }
